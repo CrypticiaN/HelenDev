f32c6f013b5400c22b70e144319793979f2a1533
CrypticiaN <obscura.darkside@gmail.com>
2022-01-22 19:50:33 +0100

    create project, working HTML

---
+++ src/main/java/org/helenjd/helendev/HelendevApplication.java
@@ -0,0 +1,13 @@
+package org.helenjd.helendev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelendevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelendevApplication.class, args);
+    }
+
+}
